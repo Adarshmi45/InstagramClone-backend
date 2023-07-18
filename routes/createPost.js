55cb5a9dc4d384a58508755e55d11daa0f8a8ce1
Adarsh Kumar <2020kucp1016@iiitkota.ac.in>
2023-07-18 13:58:37 +0530

    Delete

--- routes/createPost.js
+++ routes/createPost.js
@@ -32,6 +32,7 @@ router.post("/createPost", requireLogin, (req, res) => {
 router.get("/myposts", requireLogin, (req, res) => {
     POST.find({ postedBy: req.user._id })
         .populate("postedBy", "_id name")
+        .populate("comments.postedBy","_id name")
         .then(myposts => {
             res.json(myposts)
         })
@@ -83,6 +84,30 @@ router.put("/comment", requireLogin, async (req, res) => {
         res.status(422).json({ error: err });
     }
 });
+
+router.delete("/deletePost/:postId", requireLogin, async (req, res) => {
+    try {
+        const post = await POST.findOne({ _id: req.params.postId }).populate("postedBy", "_id");
+
+        if (!post) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+
+        if (post.postedBy._id.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ error: "You are not authorized to delete this post" });
+        }
+
+        await post.deleteOne();
+
+        res.json({ message: "Post deleted successfully" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+
 
 
 
